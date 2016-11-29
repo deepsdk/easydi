cf2e5947320fa6871cf64568a5784f85a46bc5fd
deepsdk <deepsdk.io@gmail.com>
2016-11-29 13:26:35 +0900

    input image ok

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @ViewChild("realChooseFile")
+  realChooseFile: ElementRef;
 
-  constructor() { }
+  inputImageSrc: string;
+  isDragOver: boolean = false;
+
+  constructor(private renderer: Renderer) { }
 
   ngOnInit() {
   }
 
+  onChooseFile() {
+    let evt = new MouseEvent("click", {bubbles: true});
+    this.renderer.invokeElementMethod(
+      this.realChooseFile.nativeElement, "dispatchEvent", [evt]);
+  }
+
+  readFile(file){
+    console.log(file.name);
+    console.log(file.type);
+    console.log(file.size);
+
+    let reader = new FileReader();
+    reader.onload = () => this.inputImageSrc = reader.result;
+    reader.readAsDataURL(file);
+  }
 }
